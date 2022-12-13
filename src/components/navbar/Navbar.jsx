db3fbb6889ec24711f253b83047b9eb077aba534
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-13 15:35:25 -0600

    added html funtionally to Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,2 +1,17 @@
 import React from 'react'
 import './navbar.css'
+
+const Navbar = () => {
+    const [activeNav, setActiveNav] = useState('#')
+    return (
+      <nav>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome/></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><SiAboutdotme/></a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiCodeAlt/></a>
+        <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FaServicestack/></a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><SiMinutemailer/></a>
+      </nav>
+    )
+  }
+  
+  export default Navbar
