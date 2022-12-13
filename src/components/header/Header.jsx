0064f373d7759549b621590d7ed849fc93db20a1
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-13 13:03:06 -0600

    imported Cntct-btn to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import Cntctbtn from './Cntct-btn'
 import Me from '../../assets/Me.jpg'
 
 
@@ -10,8 +11,7 @@ const Header = () => {
         <h5>Hi I'm</h5>
         <h1>Tyler Charro</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
-        <CTA/>
-        <HeaderSocial/>
+        <Cntctbtn/>
 
         <div className='me'>
           <img src={Me} alt="me" />
