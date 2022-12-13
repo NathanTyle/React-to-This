c4b8eae0532032bcaefba12c01fec37c238948bd
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-13 12:57:39 -0600

    added html functionality to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,24 @@
 import React from 'react'
 import './header.css'
 import Me from '../../assets/Me.jpg'
+
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hi I'm</h5>
+        <h1>Tyler Charro</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocial/>
+
+        <div className='me'>
+          <img src={Me} alt="me" />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
