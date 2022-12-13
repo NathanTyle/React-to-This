54be448d813ad73d0204d50ddbf1b5c941d58486
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-13 13:09:59 -0600

    imported HeaderSocials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 import Cntctbtn from './Cntct-btn'
 import Me from '../../assets/Me.jpg'
-
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -12,6 +12,7 @@ const Header = () => {
         <h1>Tyler Charro</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <Cntctbtn/>
+        <HeaderSocial/>
 
         <div className='me'>
           <img src={Me} alt="me" />
