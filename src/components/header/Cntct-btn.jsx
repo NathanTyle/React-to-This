ee836c1d61d23146f180834d6ab3318a7d44dd4c
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-13 13:00:53 -0600

    added html functionality to cntctbtn file

---
+++ src/components/header/Cntct-btn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cntctbtn = () => {
+  return (
+    <div className='cntctbtn'>
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+    </div>
+  )
+}
+
+export default Cntctbtn
