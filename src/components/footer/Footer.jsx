7758dfafad6e867209feffb67364e109038e056f
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-14 10:15:32 -0600

    added html to Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,26 @@
 import React from 'react'
 import './footer.css'
+
+const Footer = () => {
+    return (
+      <footer>
+        <a href="#" className='footer_logo'>TNC</a>
+  
+        <ul className='permalinks'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#sevices">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+  
+        <div className="footer_socials">
+          <a href="https://facebook.com"><GrFacebook/></a>
+          <a href="https://instagram.com"><BsInstagram/></a>
+        </div>
+      </footer>
+    )
+  }
+  
 export default Footer
