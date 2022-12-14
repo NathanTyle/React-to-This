45e9991e80981b6d8bc6e59c21146fcd6a07161c
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-14 11:27:40 -0600

    added header_socials-icon class and styles

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin className='header_socials-icon'/></a>
+        <a href="https://github.com" target="_blank"><BsGithub className='header_socials-icon'/></a>
     </div>
   )
 }
