ec4cc584b5ded130ef8fa1eac0e5c1cd08ff0350
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-14 10:01:43 -0600

    added html to Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,3 +1,37 @@
 import React from 'react'
 import './portfolio.css'
+
+const Portfolio = () => {
+    return (
+      <section id='portfolio'>
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+  
+        <div className='container portfolio_container'>
+          <article className='portfolio_item'>
+            <div className='portfolio_item-image'>
+              <img src={IMG1} alt="Anination" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="https://hieuwy.github.io/project-1/" className='btn btn-primary'>Github</a>
+          </article>
+          <article className='portfolio_item'>
+            <div className='portfolio_item-image'>
+              <img src={IMG2} alt="Anination" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="https://listen-to-me512.herokuapp.com/" className='btn btn-primary'>Github</a>
+          </article>
+          <article className='portfolio_item'>
+            <div className='portfolio_item-image'>
+              <img src={IMG3} alt="Anination" />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="https://escentials.herokuapp.com/" className='btn btn-primary'>Github</a>
+          </article>
+        </div>
+      </section>
+    )
+  }
+  
 export default Portfolio
